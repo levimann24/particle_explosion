85e695fcdd2f7b5299b59048fbb667c2b5c17593
Levi Mann <levimann@MacBook-Pro.attlocal.net>
2020-06-15 10:50:29 -0500

    First commit

---
+++ settings.py
@@ -0,0 +1,14 @@
+class Settings:
+    def __init__(self):
+
+        # Define the window
+        self.WIDTH = 800
+        self.HEIGHT = 800
+        self.bg_color = (100, 100, 100)
+
+        # define some particle parameters
+        self.p_w = 1
+        self.p_h = 1
+        self.p_speed = 1
+        self.p_acc = 1
+        self.p_color = (255, 0, 0)
